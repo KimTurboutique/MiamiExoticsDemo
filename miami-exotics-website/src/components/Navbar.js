014d7971d701b709297a933f6b991cf9d155821d
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2024-01-18 14:50:18 -0500

    added components: navbar, btn

--- miami-exotics-website/src/components/Navbar.js
+++ miami-exotics-website/src/components/Navbar.js
@@ -2,16 +2,39 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-            MIAMI EXOTIC RENTALS LOGO HERE
+            MIAMI EXOTIC RENTALS LOGO HERE <i className='fa-solid fa-car'></i>
         </Link>
-        <div className='menu-icon'>
-          
+        <div className='menu-icon' onClick={handleClick}>
+        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+              </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/catalog' className='nav-links' onClick={closeMobileMenu}>
+              Catalog
+              </Link>
+              <li className='nav-item'>
+            <Link to='/sign-up' className='-nav-links-mobile' onClick={closeMobileMenu}>
+              Sign Up
+              </Link>
+          </li>
+          </li>
+        </ul>
+        
         </div>
     </nav>  
     </>
