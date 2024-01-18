bafe0147053710e779f5bdc622d88bb6226793e9
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2024-01-18 14:20:20 -0500

    fixed bug with switch, updated switch to use routes. added font awesome kit

--- miami-exotics-website/src/components/Navbar.js
+++ miami-exotics-website/src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
-        
+        <Link to="/" className="navbar-logo">
+            MIAMI EXOTIC RENTALS LOGO HERE
+        </Link>
+        <div className='menu-icon'>
+          
+        </div>
         </div>
     </nav>  
     </>
